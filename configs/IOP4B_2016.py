f77352347a25e5f476e65f5ff18d5f81f5316642
Dan Dawson <dawson29@purdue.edu>
2020-03-21 15:12:10 -0400

    Added some XTRRA-related stuff from Robin

---
+++ configs/IOP4B_2016.py
@@ -0,0 +1,44 @@
+"""Configuration script for VORTEX-SE IOP4B 2016 deployment"""
+
+PIPS_IO_dict = {
+    'dataset_name': 'IOP4B_2016',
+    'deployment_names': ['IOP4B_D1_2016'] * 4,
+    'PIPS_dir': '/Users/dawson29/sshfs_mounts/depot/data/Projects/VORTEXSE/obsdata/full_PIPS_dataset',
+    'plot_dir': '/Users/dawson29/sshfs_mounts/depot/data/Projects/VORTEXSE/obsdata/full_PIPS_dataset/plots',
+    'PIPS_types': ['PIPS'] * 4,
+    'PIPS_names': ['PIPS1A', 'PIPS1B', 'PIPS2A', 'PIPS2B'],
+    'PIPS_filenames': ['PIPS1A_2016_IOP4B_D1.txt', 'PIPS1B_2016_IOP4B_D1.txt',
+                       'PIPS2A_2016_IOP4B_D1.txt', 'PIPS2B_2016_IOP4B_D1.txt'],
+    'start_times': [None] * 4,
+    'end_times': [None] * 4,
+    'requested_interval': 60.
+}
+
+PIPS_qc_dict = {
+    'strongwindQC': True,
+    'splashingQC': True,
+    'marginQC': True,
+    'rainfallQC': True,
+    'rainonlyQC': True,
+    'hailonlyQC': False,
+    'graupelonlyQC': False,
+    'basicQC': False,
+}
+
+radar_config_dict = {
+    'load_radar_at_PIPS': True,
+    'save_radar_at_PIPS': False,
+    'comp_radar': True,
+    'clean_radar': False,
+    'calc_dualpol': True,
+    'plot_retrieval': True,
+    'radar_name': 'KGWX',
+    'radar_type': 'NEXRAD',
+    'radar_dir': '/Users/dawson29/sshfs_mounts/depot/data/Projects/VORTEXSE/obsdata/2016/NEXRAD/IOP_4B/CFRadial/',
+    'field_names': ['REF', 'ZDR', 'RHO'],
+    'el_req': 0.5,
+    'radar_start_timestamp': '20160429210000',
+    'radar_end_timestamp': '20160430000500',
+    'scatt_dir': '/Users/dawson29/Projects/pyPIPS/tmatrix/S-Band/',
+    'wavelength': 10.7
+}
