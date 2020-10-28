8ac5f9ba15724d3e5644dd58db5ff87f513398a4
Connor Belak <cbelak@purdue.edu>
2020-10-28 13:56:24 -0400

    Connor PIPS Plotting Notebooks

--- configs/IOP4C_2017_3_PIPS_CCN_100.py
+++ configs/IOP4C_2017_3_PIPS_CCN_100.py
@@ -1,10 +1,10 @@
-"""Configuration script for VORTEX-SE IOP4C 2017 deployment 1-km CCN 100"""
+"""Configuration script for VORTEX-SE IOP4C 2017 deployment 1-km CCN 1500 with adjusted EPS"""
 
 PIPS_IO_dict = {
     'dataset_name': 'IOP4C_2017',
     'deployment_names': ['IOP4C_D1_2017'] * 3,
     'PIPS_dir': '/depot/dawson29/data/Projects/VORTEXSE/obsdata/full_PIPS_dataset_RB15/',
-    'plot_dir': '/depot/dawson29/data/Projects/VORTEXSE/simulations/ARPS/2017_IOP4C/EnKF/PIPS/plots/CCN100',
+    'plot_dir': '/depot/dawson29/data/Projects/VORTEXSE/simulations/ARPS/2017_IOP4C/EnKF/PIPS/New/plots/full_time/CCN100/',
     'PIPS_types': ['PIPS'] * 3,
     'PIPS_names': ['PIPS1A', 'PIPS1B', 'PIPS2B'],
     'PIPS_filenames': ['PIPS1A_2017_IOP4C_D1.txt', 'PIPS1B_2017_IOP4C_D1.txt',
@@ -41,20 +41,20 @@ radar_config_dict = {
     'field_names': ['REF', 'ZDR', 'RHO'],
     'el_req': 0.5,
     'radar_start_timestamp': '20170430170000',
-    'radar_end_timestamp': '20170430210500',
+    'radar_end_timestamp': '20170430220000',
     'scatt_dir': '/home/cbelak/pyPIPS/tmatrix/S-Band/',
     'wavelength': 10.7
 }
 
 model_config_dict = {
-    'runname': '5_min_CCN_100_1km243x243_3km153x153_043017_NAM',
+    'runname': 'CCN_100_1km243x243_3km153x153_043017_NAM',
     'nens': 40,
     'fileformat': 'hdf',
     'microphys': 'ZVD',
     'model_dt': 60.,
     'model_dt_mean': 300.,
     'cycle': 'prior',
-    'basedirname': '/scratch/rice/c/cbelak/Projects/VORTEXSE/simulations/ARPS/2017_IOP4C/EnKF/1km243x243_3km153x153_043017_CCN100',
+    'basedirname': '/scratch/rice/c/cbelak/Projects/VORTEXSE/simulations/ARPS/2017_IOP4C/EnKF/1km243x243_3km153x153_043017_CCN100_New/',
     'timestamp_model_init': '20170430060000',
     'timestamp_model_start': '20170430181500',
     'timestamp_model_stop': '20170430201500',
--- configs/IOP4C_2017_3_PIPS_CCN_500.py
+++ configs/IOP4C_2017_3_PIPS_CCN_500.py
@@ -4,7 +4,7 @@ PIPS_IO_dict = {
     'dataset_name': 'IOP4C_2017',
     'deployment_names': ['IOP4C_D1_2017'] * 3,
     'PIPS_dir': '/depot/dawson29/data/Projects/VORTEXSE/obsdata/full_PIPS_dataset_RB15/',
-    'plot_dir': '/depot/dawson29/data/Projects/VORTEXSE/simulations/ARPS/2017_IOP4C/EnKF/PIPS/full_time/plots/CCN500/',
+    'plot_dir': '/depot/dawson29/data/Projects/VORTEXSE/simulations/ARPS/2017_IOP4C/EnKF/PIPS/New/plots/full_time/CCN1500/',
     'PIPS_types': ['PIPS'] * 3,
     'PIPS_names': ['PIPS1A', 'PIPS1B', 'PIPS2B'],
     'PIPS_filenames': ['PIPS1A_2017_IOP4C_D1.txt', 'PIPS1B_2017_IOP4C_D1.txt',
@@ -41,20 +41,20 @@ radar_config_dict = {
     'field_names': ['REF', 'ZDR', 'RHO'],
     'el_req': 0.5,
     'radar_start_timestamp': '20170430170000',
-    'radar_end_timestamp': '20170430210500',
+    'radar_end_timestamp': '20170430220000',
     'scatt_dir': '/home/cbelak/pyPIPS/tmatrix/S-Band/',
     'wavelength': 10.7
 }
 
 model_config_dict = {
-    'runname': '1km243x243_3km153x153_043017_NAM_CCN500',
+    'runname': 'CCN_500_1km243x243_3km153x153_043017_NAM',
     'nens': 40,
     'fileformat': 'hdf',
     'microphys': 'ZVD',
     'model_dt': 60.,
     'model_dt_mean': 300.,
     'cycle': 'prior',
-   'basedirname': '/scratch/rice/c/cbelak/Projects/VORTEXSE/simulations/ARPS/2017_IOP4C/EnKF/1km243x243_3km153x153_043017_CCN500/',
+    'basedirname': '/scratch/rice/c/cbelak/Projects/VORTEXSE/simulations/ARPS/2017_IOP4C/EnKF/1km243x243_3km153x153_043017_CCN500_New/',
     'timestamp_model_init': '20170430060000',
     'timestamp_model_start': '20170430181500',
     'timestamp_model_stop': '20170430201500',
