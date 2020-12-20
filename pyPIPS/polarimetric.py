a8be9822188022d91dcc9a2812b3172e91ae2975
Dan Dawson <dawson29@purdue.edu>
2020-12-20 16:07:12 -0500

    Added notebooks for size sorting trajectory model

--- pyPIPS/polarimetric.py
+++ pyPIPS/polarimetric.py
@@ -82,3 +82,55 @@ def calpolrain(wavelength, filename, Nd, intv):
                     'KDP': Kdp, 'RHO': rhv, 'intv': intv, 'd': d, 'fa2': fa2, 'fb2': fb2}
 
     return dualpol_dict
+
+def calpolrain_xr(wavelength, filename, Nd, intv):
+    """Given backscattering amplitudes and a discrete distribution N(D) (m^-4), compute
+       polarimetric variables for each bin."""
+
+    d, far_b, fbr_b, far_f, fbr_f = readtmatrix(filename)
+    fa2, fb2, fab, fba, far = calbackscatterrain(far_b, fbr_b, far_f, fbr_f)
+
+    # There may be more bins in the given Nd than are read in from the file.
+    # This is because the Nd contains bins above the maximum size of rain (~9 mm).
+    # Truncate the diameter dimension of Nd to account for this.
+    # Also transpose Nd to allow for numpy broadcasting
+    # Nd = Nd[:, :np.size(fa2)] # Nd[:np.size(fa2), :].T
+    Nd = Nd.isel(diameter_bin=slice(0, np.size(fa2)))
+    intv = intv[:np.size(fa2)]
+
+    lamda = wavelength * 10.  # Get radar wavelength in mm
+    Kw2 = 0.93  # Dielectric factor for water
+    sar_h = fa2 * Nd * intv
+    sar_v = fb2 * Nd * intv
+    sar_hv = fab * Nd * intv
+    fsar = far * Nd * intv
+
+    # TODO: return binned values (by diameter) in addition to total
+    Zh_bin = 4. * lamda**4. / (np.pi**4. * Kw2) * sar_h
+    Zv_bin = 4. * lamda**4. / (np.pi**4. * Kw2) * sar_v
+    Zhv_bin = 4. * lamda**4. / (np.pi**4. * Kw2) * sar_hv
+    Kdp_bin = 180. * lamda / np.pi * fsar * 1.e-3
+    dBZ_bin = 10. * np.log10(Zh_bin)
+    ZDR_lin_bin = Zh_bin / Zv_bin
+    ZDR_bin = 10. * np.log10(np.maximum(1.0, ZDR_lin_bin))
+    temp = Zh_bin * Zv_bin
+    # Added by Jess (was temp > 0).  Find out why...
+    rhv_bin = np.where(Zh_bin != Zv_bin, Zhv_bin / (np.sqrt(temp)), np.nan)
+    Zh = Zh_bin.sum(dim='diameter_bin') # np.sum(Zh_bin, axis=1)
+    Zv = Zv_bin.sum(dim='diameter_bin') # np.sum(Zv_bin, axis=1)
+    Zhv = np.abs(Zhv_bin.sum(dim='diameter_bin'))
+    Kdp = Kdp_bin.sum(dim='diameter_bin')
+    dBZ = 10. * np.log10(Zh)
+    temp = Zh / Zv
+    ZDR = 10. * np.log10(np.maximum(1.0, temp))
+    temp = Zh * Zv
+    # Added by Jess (was temp > 0).  Find out why...
+    rhv = np.where(Zh != Zv, Zhv / (np.sqrt(temp)), np.nan)
+    # np.savetxt('temp.txt', temp)
+
+    dualpol_dict = {'ZH_bin': Zh_bin, 'ZV_bin': Zv_bin, 'ZHV_bin': Zhv_bin, 'REF_bin': dBZ_bin,
+                    'ZDR_lin_bin': ZDR_lin_bin, 'ZDR_bin': ZDR_bin, 'KDP_bin': Kdp_bin,
+                    'RHO_bin': rhv_bin, 'ZH': Zh, 'ZV': Zv, 'ZHV': Zhv, 'REF': dBZ, 'ZDR': ZDR,
+                    'KDP': Kdp, 'RHO': rhv, 'intv': intv, 'd': d, 'fa2': fa2, 'fb2': fb2}
+
+    return dualpol_dict
